f85d8428722b5edb51e0746964cbca3ff763d4c2
SuciuFlaviu <flaviusuciuflorian@gmail.com>
2017-04-27 19:56:52 +0300

    detectare fata

--- video.cpp
+++ video.cpp
@@ -20,11 +20,25 @@ int main(void)
 		return 1;
 	}
 
+	CascadeClassifier c;
+	c.load(E:\P.A.B.D An.I.sem II\Programare Android - Mihai Olteanu\opencv\sources\data\haarcascades\haarcascade_frontalface_alt.xml);
+
+
+
 	namedWindow("camera");
+
+	vector<Rect> capete;
+	
+
 
 	while (1){
 
 		video >> a;
+		c.detectMultiScale(a, capete);
+
+		for (int i = 0; i < capete.size(); i++);
+		rectangle(a, capete[i], Scalar(255, 0, 0));
+
 		imshow("camera", a);
 		if (waitKey(1) == 27);
 		break;
